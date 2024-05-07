8caa7dfb4d2eb9e63dd0277bd335f4da9a32352a
Igor Kevin <igor_kevin@hotmail.com>
2024-05-07 19:27:20 -0300

    Usuarios criado com suas relativas pastas, usnado o comando: "nest g res usuarios"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: ['.env'],
     isGlobal: true,
   }
-  )],
+  ), UsuariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
